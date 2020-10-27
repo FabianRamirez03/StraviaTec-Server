ba172237fbc4570974027957dd3cc5ddf0df0b31
Mario Araya <marioaxtrem@gmail.com>
2020-10-27 14:17:19 -0600

    creacion de tablas desde visual studio

---
+++ ServerStraviaTec/Clases/DatosUsuarios.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ServerStraviaTec.Models;
+
+namespace ServerStraviaTec.Clases
+{
+    public class DatosUsuarios : DbContext
+    {
+        public DatosUsuarios (DbContextOptions <DatosUsuarios> options) : base(options) { }
+
+        public DbSet<Deportista> Deportista { get; set; }
+    }
+}
---
+++ ServerStraviaTec/Models/Deportista.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerStraviaTec.Models
+{
+    public class Deportista
+    {
+        [Key]
+        public int Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Usuario { get; set; }
+        public string Password { get; set; }
+
+        public Deportista()
+        {
+        }
+        
+        public Deportista(string NombreDeportista, string ApellidoDeportista, string UsuarioDeportista, string PasswordDeportista, int CedulaDeportista)
+        {
+            Cedula = CedulaDeportista;
+            Apellido = ApellidoDeportista;
+            Usuario = UsuarioDeportista;
+            Password = PasswordDeportista;
+            Cedula = CedulaDeportista;
+
+        }
+    }
+}

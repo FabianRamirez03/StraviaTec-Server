99ebd2685b197b6dbf352b5b38cd7c276c9ded7b
Mariana Vargas Ramirez <marianavargas11209@gmail.com>
2020-11-15 00:43:47 -0600

    [edit] usuario contoller

--- APIStraviaTec/APIStraviaTec/Controllers/UsuarioController.cs
+++ APIStraviaTec/APIStraviaTec/Controllers/UsuarioController.cs
@@ -48,7 +48,7 @@ namespace APIStraviaTec.Controllers
             return new string[] {valor, "value2" };
         }
 
-        [Route("usuarioid")]
+        [Route("porUsuarioId")]
         [EnableCors("AnotherPolicy")]
         [HttpPost]
         public List<Usuario> PostUsuarioId([FromBody] Usuario usuario)
@@ -63,14 +63,11 @@ namespace APIStraviaTec.Controllers
             command.Parameters.AddWithValue("@usuarioid", NpgsqlTypes.NpgsqlDbType.Integer, usuario.Idusuario);
             // Execute the query and obtain a result set
             NpgsqlDataReader dr = command.ExecuteReader();
-            if(dr==null)
-            {
-                Debug.WriteLine("Producto no encontrado");
-            }
-            else
+            try
             {
                 while (dr.Read())
                 {
+                    usuario.Idusuario = (int)dr[0];
                     usuario.Nombreusuario = dr[1].ToString();
                     usuario.Contrasena = dr[2].ToString();
                     usuario.Primernombre = dr[3].ToString();
@@ -81,7 +78,7 @@ namespace APIStraviaTec.Controllers
                     {
                         usuario.Foto = null;
                     }
-                    else 
+                    else
                     {
                         usuario.Foto = (byte[])dr[7];
                     }
@@ -93,7 +90,12 @@ namespace APIStraviaTec.Controllers
 
 
                 }
-                
+
+            }
+            catch
+            {
+                Debug.WriteLine("Producto no encontrado");
+
             }
             conn.Close();
             return Usuarioret;
@@ -123,5 +125,158 @@ namespace APIStraviaTec.Controllers
             conn.Close();
             return Usuarioret;
         }
-    }
+
+        [Route("porUsuarioName")]
+        [EnableCors("AnotherPolicy")]
+        [HttpPost]
+        public List<Usuario> PostUsuarioName([FromBody] Usuario usuario)
+        {
+            List<Usuario> Usuarioret = new List<Usuario>();
+            //Connect to a PostgreSQL database
+            NpgsqlConnection conn = new NpgsqlConnection(serverKey);
+            conn.Open();
+            // Define a query returning a single row result set 
+            NpgsqlCommand command = new NpgsqlCommand("buscarUsuarioNombre", conn);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@nombre", NpgsqlTypes.NpgsqlDbType.Varchar, usuario.Primernombre);
+            // Execute the query and obtain a result set
+            NpgsqlDataReader dr = command.ExecuteReader();
+            try
+            {
+                while (dr.Read())
+                {
+                    usuario.Idusuario = (int)dr[0];
+                    usuario.Nombreusuario = dr[1].ToString();
+                    usuario.Contrasena = dr[2].ToString();
+                    usuario.Primernombre = dr[3].ToString();
+                    usuario.Apellidos = dr[4].ToString();
+                    usuario.Fechanacimiento = (DateTime)dr[5];
+                    usuario.Nacionalidad = dr[6].ToString();
+                    if (dr[7] == DBNull.Value)
+                    {
+                        usuario.Foto = null;
+                    }
+                    else
+                    {
+                        usuario.Foto = (byte[])dr[7];
+                    }
+                    usuario.Carrera = null;
+                    usuario.Reto = null;
+                    usuario.Grupo = null;
+                    string json = JsonConvert.SerializeObject(usuario);
+                    Usuarioret.Add(usuario);
+
+
+                }
+
+            }
+            catch
+            {
+                Debug.WriteLine("Producto no encontrado");
+
+            }
+            conn.Close();
+            return Usuarioret;
+        }
+        [Route("porUsuarioNP")]
+        [EnableCors("AnotherPolicy")]
+        [HttpPost]
+        public List<Usuario> PostUsuarioNP([FromBody] Usuario usuario)
+        {
+            List<Usuario> Usuarioret = new List<Usuario>();
+            //Connect to a PostgreSQL database
+            NpgsqlConnection conn = new NpgsqlConnection(serverKey);
+            conn.Open();
+            // Define a query returning a single row result set 
+            NpgsqlCommand command = new NpgsqlCommand("buscarUsuarioNombreApellido", conn);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@nombre", NpgsqlTypes.NpgsqlDbType.Varchar, usuario.Primernombre);
+            command.Parameters.AddWithValue("@apellido", NpgsqlTypes.NpgsqlDbType.Varchar, usuario.Apellidos);
+            // Execute the query and obtain a result set
+            NpgsqlDataReader dr = command.ExecuteReader();
+            try
+            {
+                while (dr.Read())
+                {
+                    usuario.Idusuario = (int)dr[0];
+                    usuario.Nombreusuario = dr[1].ToString();
+                    usuario.Contrasena = dr[2].ToString();
+                    usuario.Primernombre = dr[3].ToString();
+                    usuario.Apellidos = dr[4].ToString();
+                    usuario.Fechanacimiento = (DateTime)dr[5];
+                    usuario.Nacionalidad = dr[6].ToString();
+                    if (dr[7] == DBNull.Value)
+                    {
+                        usuario.Foto = null;
+                    }
+                    else
+                    {
+                        usuario.Foto = (byte[])dr[7];
+                    }
+                    usuario.Carrera = null;
+                    usuario.Reto = null;
+                    usuario.Grupo = null;
+                    string json = JsonConvert.SerializeObject(usuario);
+                    Usuarioret.Add(usuario);
+                }
+
+            }
+            catch 
+            {
+                Debug.WriteLine("Producto no encontrado");
+
+            }
+            
+            conn.Close();
+            return Usuarioret;
+        }
+        [Route("modificarUsuario")]
+        [EnableCors("AnotherPolicy")]
+        [HttpPost]
+        public List<Usuario> PostmodificarUsuario([FromBody] Usuario usuario)
+        {
+            List<Usuario> Usuarioret = new List<Usuario>();
+            //Connect to a PostgreSQL database
+            NpgsqlConnection conn = new NpgsqlConnection(serverKey);
+            conn.Open();
+            // Define a query returning a single row result set 
+            NpgsqlCommand command = new NpgsqlCommand("modificarUsuario", conn);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@iduser", NpgsqlTypes.NpgsqlDbType.Integer, usuario.Idusuario);
+            command.Parameters.AddWithValue("@username", NpgsqlTypes.NpgsqlDbType.Varchar, usuario.Nombreusuario);
+            command.Parameters.AddWithValue("@contra", NpgsqlTypes.NpgsqlDbType.Varchar, usuario.Contrasena);
+            command.Parameters.AddWithValue("@nombre", NpgsqlTypes.NpgsqlDbType.Varchar, usuario.Primernombre);
+            command.Parameters.AddWithValue("@apellido", NpgsqlTypes.NpgsqlDbType.Varchar, usuario.Apellidos);
+            command.Parameters.AddWithValue("@nacimiento", NpgsqlTypes.NpgsqlDbType.Date, usuario.Fechanacimiento);
+            command.Parameters.AddWithValue("@pais", NpgsqlTypes.NpgsqlDbType.Varchar, usuario.Nacionalidad);
+            // Execute the query and obtain a result set
+            command.ExecuteNonQuery();
+            Debug.WriteLine("Usuario creado exitosamente");
+            conn.Close();
+            return Usuarioret;
+        }
+
+        [Route("eliminarUsuario")]
+        [EnableCors("AnotherPolicy")]
+        [HttpPost]
+        public List<Usuario> PosteliminarUsuario([FromBody] Usuario usuario)
+        {
+            List<Usuario> Usuarioret = new List<Usuario>();
+            //Connect to a PostgreSQL database
+            NpgsqlConnection conn = new NpgsqlConnection(serverKey);
+            conn.Open();
+            // Define a query returning a single row result set 
+            NpgsqlCommand command = new NpgsqlCommand("eliminarUsuarioNombre", conn);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@username", NpgsqlTypes.NpgsqlDbType.Varchar, usuario.Nombreusuario);
+            // Execute the query and obtain a result set
+            command.ExecuteNonQuery();
+            Debug.WriteLine("Usuario eliminado exitosamente");
+            conn.Close();
+            return Usuarioret;
+        }
+
+
+
+    }  
 }
